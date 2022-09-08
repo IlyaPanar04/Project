ef8def90fe41c4fd842d74ff07e38d7862fc3c92
IlyaPanar04 <ilpanard58@gmail.com>
2022-09-08 20:20:19 +0300

    Update!

---
+++ button.js
@@ -0,0 +1,123 @@
+/*function tick() {
+        const element = (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {new Date().toLocaleTimeString()}.</h2>
+            </div>
+        );
+        root.render(element);
+    };
+    setInterval(tick , 1000)*/
+
+
+
+
+/* function Welcome(props) {
+        return <h1>Привет, {props.name}</h1>;
+    }
+    const user = {name:'Ilya'}
+    const element = Welcome(user)
+    root.render(element)*/
+
+
+/*function Welcome(props) {
+        return <h1>Привет, {props.name}</h1>;
+    }
+
+    function App(){
+        return (
+            <div>
+                <Welcome name = 'Ilya'/>
+                <Welcome name = 'Darya'/>
+                <Welcome name = 'Nikita'/>
+            </div>
+        )
+    }
+    root.render(<App/>)*/
+
+
+
+
+/* function Clock(props) {
+        return(
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {props.date.toLocaleTimeString()}.</h2>
+            </div>
+        )
+
+    }
+
+    function tick(){
+        root.render(<Clock date = {new Date()}/>)
+    }
+
+    setInterval(tick , 1000)*/
+
+
+
+
+/*class Clock extends React.Component{
+        constructor(props) {
+            super(props);
+            this.state = {date: new Date()}
+        }
+
+        componentDidMount() {
+            this.timerID = setInterval(
+                () => this.tick(),
+                1000
+            );
+        }
+
+        componentWillUnmount() {
+            clearInterval(this.timerID);
+        }
+
+        tick(){
+            this.setState({
+                date : new Date()
+            })
+        }
+
+        render(){
+            return(
+                <div>
+                    <h1>Hello, world!</h1>
+                    <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+                </div>
+            )
+        }
+    }
+
+    root.render(<Clock />)*/
+
+
+
+/*class Toggle extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {isToggleOn: true};
+
+            // Эта привязка обязательна для работы `this` в колбэке.
+            this.handleClick = this.handleClick.bind(this);
+        }
+
+        handleClick() {
+            this.setState(prevState => ({
+                isToggleOn: !prevState.isToggleOn
+            }));
+        }
+
+        render() {
+            return (
+                <button onClick={this.handleClick}>
+                    {this.state.isToggleOn ? 'Включено' : 'Выключено'}
+                </button>
+            );
+        }
+    }
+
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(<Toggle />);*/
+
